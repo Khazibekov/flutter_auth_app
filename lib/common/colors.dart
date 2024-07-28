762dc836eaf96f7a110a07024a1e8ccf274e2bfa
Esref38 <esrefqazibeyov0538@gmail.com>
2024-07-28 22:46:09 +0400

    signIn

---
+++ lib/common/colors.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+const containerColor = Color(0xFFFFFFFF);
+const titleColor = Color(0xFF000000);
--- lib/features/auth/views/sign_in.dart
+++ lib/features/auth/views/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/common/colors.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -11,7 +12,44 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/sign_in.png"),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: containerColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  )),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                      child: Text(
+                    "Sign In",
+                    style: TextStyle(
+                      color: titleColor,
+                      fontSize: 24,
+                    ),
+                  ))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
